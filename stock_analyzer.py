e51e3c5770f623559a76503459f5f7a2bc0c7229
Longer <daniel199766@gmail.com>
2023-11-19 21:56:28 +0800

    refactor : revise code and add type hint

--- stock_analyzer.py
+++ stock_analyzer.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
 import os
-import datetime
 import matplotlib.pyplot as plt
 import tw_stock_id
 from sklearn.linear_model import LinearRegression
-from common import DATA_START_YEAR, DATA_END_YEAR
+from common import TREND_START_YEAR, TREND_END_YEAR, DataType, LoadedData, BALANCE_FEATURES, PROFIT_FEATURES, PROFIT_RAW_DATA_COLUMNS, BALANCE_RAW_DATA_COLUMNS
+
+DATA_LIST = [DataType.dividend_history, DataType.profit_indicator, DataType.balance_sheet, DataType.daily_close]
 
 class StockAnalizer :
     
@@ -39,8 +40,6 @@ class StockAnalizer :
         # Coeficient
         w_1 = round(model.coef_[0], 2)
         
-        
-        
         prediction = model.predict(x)
         error = prediction - y
         rmse = (error**2).mean()**0.5
@@ -64,14 +63,10 @@ class StockAnalizer :
         
         plt.clf()
     
-    def process_balance_sheet(self, balance_sheet):
+    def process_balance_sheet(self, balance_sheet:pd.DataFrame) -> pd.DataFrame:
         
-        balance_sheet = balance_sheet[["years", "stock" , "current_assets", "current_liabilities"]]
-        
-        for column in balance_sheet.columns:
-            # special condition
-            balance_sheet.drop(balance_sheet[balance_sheet[column] == "-"].index, inplace = True)
-        balance_sheet = balance_sheet.astype(float)
+        balance_sheet = balance_sheet[BALANCE_RAW_DATA_COLUMNS]
+        balance_sheet = self.drop_no_value_rows(data=balance_sheet)
         
         # sort data by year in descending order
         balance_sheet = balance_sheet[::-1]
@@ -79,23 +74,28 @@ class StockAnalizer :
         
         balance_sheet["current_ratio(%)"] = (balance_sheet["current_assets"] / balance_sheet["current_liabilities"] * 100).round(2)
         balance_sheet["quick_ratio(%)"]   = ((balance_sheet["current_assets"] - balance_sheet["stock"]) / balance_sheet["current_liabilities"] * 100).round(2)
+        balance_sheet = balance_sheet[(TREND_START_YEAR<=balance_sheet['years']) & (balance_sheet['years'] <= TREND_END_YEAR) ].reset_index(drop=True)
         
         return balance_sheet
     
+    def drop_no_value_rows(self, data:pd.DataFrame) -> pd.DataFrame:
+        
+        for column in data.columns:
+            data.drop(data[data[column] == "-"].index, inplace = True)
+        
+        return data.astype(float)
     
-    def process_profit_indicator(self, profit_indicator, stk_id):
+    def process_profit_indicator(self, profit_indicator:pd.DataFrame) -> pd.DataFrame:
         
-        profit_indicator = profit_indicator[["years", "revenue(E)" ,"gross_profit(%)" , "operating_margin(%)" , "other_income(%)" , "net_income(%)" , "ROE(%)" , "ROA(%)", "EPS", "up(dolar)", "BPS"]]
-        
-        for column in profit_indicator.columns:
-            profit_indicator.drop(profit_indicator[profit_indicator[column] == "-"].index, inplace = True)
-        
-        profit_indicator = profit_indicator.astype(float)
+        profit_indicator = profit_indicator[PROFIT_RAW_DATA_COLUMNS]
+        profit_indicator = self.drop_no_value_rows(data=profit_indicator)
         
         # sort data by year in descending order
         profit_indicator = profit_indicator[::-1]
         profit_indicator.reset_index(drop=True, inplace=True)
-        profit_indicator['revenue(%)'] = round(profit_indicator['revenue(E)'] / profit_indicator['revenue(E)'].head(1)[0], 2)
+        profit_indicator['revenue(%)'] = round(profit_indicator['revenue(100B)'] / profit_indicator['revenue(100B)'].head(1)[0], 2)
+        
+        profit_indicator = profit_indicator[(TREND_START_YEAR<=profit_indicator['years']) & (profit_indicator['years'] <= TREND_END_YEAR) ].reset_index(drop=True)
         
         return profit_indicator
         
@@ -104,32 +104,23 @@ class StockAnalizer :
         dividend_history = dividend_history[["years","cash_dividend_yield(%)"]].astype(float).round(2)
         return dividend_history
     
-    def analyze_data(self, stk_list):
-        
-        # profit_features = ["revenue(%)" ,"gross_profit(%)" , "operating_margin(%)" , "other_income(%)" , "net_income(%)"]
-        # balance_features = ["current_ratio(%)", "quick_ratio(%)"]
-        profit_features = ["revenue(%)", "gross_profit(%)", "net_income(%)"]
+    def analyze_data(self, stk_list:list[str]):
 
         for stk_id in stk_list :     
             print(stk_id)
-            dividend_history, profit_indicator, balance_sheet, daily_close = self.load_data(stk_id=stk_id)
+            loaded_data = self.load_data(stk_id=stk_id)
             
             # dividend_history = self.process_dividend_history(dividend_history=dividend_history)
-            profit_indicator = self.process_profit_indicator(profit_indicator=profit_indicator.copy(), stk_id=stk_id)
-            balance_sheet = self.process_balance_sheet(balance_sheet=balance_sheet.copy())
+            profit_indicator = self.process_profit_indicator(profit_indicator=loaded_data.profit_indicator.copy())
+            balance_sheet = self.process_balance_sheet(balance_sheet=loaded_data.balance_sheet.copy())
             
-            filted_profit_indicator = profit_indicator[(DATA_START_YEAR<=profit_indicator['years']) & (profit_indicator['years'] <= DATA_END_YEAR) ].reset_index(drop=True)
-            filted_balance_sheet = balance_sheet[(DATA_START_YEAR<=balance_sheet['years']) & (balance_sheet['years'] <= DATA_END_YEAR)].reset_index(drop=True)
-            
-            if not filted_profit_indicator.empty:
-                if (filted_profit_indicator['years'][0] == DATA_START_YEAR) &  (filted_balance_sheet['years'][0] == DATA_START_YEAR):
-                    # profit_features_mae = self.plot_trend_line(stk_id=stk_id, data=profit_indicator, features=profit_features)
-                    # balance_features_mae = self.plot_trend_line(stk_id=stk_id, data=balance_sheet, features=balance_features)
-                    # self.performance = pd.concat([self.performance, pd.DataFrame({**profit_features_mae, **balance_features_mae}, index=[stk_id])])
-                    profit_features_mae = self.plot_trend_line(stk_id=stk_id, data=filted_profit_indicator, features=profit_features)
-                    self.performance = pd.concat([self.performance, pd.DataFrame({**profit_features_mae}, index=[stk_id])])
-                    self.gen_rolling_trend_line(stk_id=stk_id, data=profit_indicator, features=profit_features)
-                    
+            if not profit_indicator.empty and not balance_sheet.empty and ((profit_indicator['years'][0] == TREND_START_YEAR) & (balance_sheet['years'][0] == TREND_START_YEAR)):
+                # balance_features_mae = self.plot_trend_line(stk_id=stk_id, data=balance_sheet, features=BALANCE_FEATURES)
+                profit_features_mae = self.gen_trend(data=profit_indicator, features=PROFIT_FEATURES)
+                self.plot_trend_line(stk_id=stk_id, data=profit_indicator, features=PROFIT_FEATURES)
+                # self.performance = pd.concat([self.performance, pd.DataFrame({**profit_features_mae, **balance_features_mae}, index=[stk_id])])
+                self.performance = pd.concat([self.performance, pd.DataFrame({**profit_features_mae}, index=[stk_id])])
+                self.gen_rolling_trend_line(stk_id=stk_id, data=profit_indicator, features=PROFIT_FEATURES)
                 
         performance_folder = os.path.join(self.data_folder, 'performance')
         if not os.path.exists(performance_folder):
@@ -137,18 +128,15 @@ class StockAnalizer :
             
         self.performance.to_csv(os.path.join(performance_folder, 'performance.csv'))
 
-    def gen_rolling_trend_line(self, stk_id, data, features):
+    def gen_rolling_trend_line(self, stk_id:str, data:pd.DataFrame, features:list[str]):
         
-        data = data[(DATA_START_YEAR<=data['years'])].reset_index(drop=True)
-        
+        data = data[(TREND_START_YEAR<=data['years'])].reset_index(drop=True)
         rolling_data = list(data.rolling(window=10))[9:]
         
         rolling_trend = pd.DataFrame()
         
         for data in rolling_data:
-            
             data = data.reset_index(drop=True)
-            
             rolling_result = {}
             for feature in features :
                 coef, mae = self.predict(data=data, x_label="years", y_label=feature)
@@ -161,33 +149,36 @@ class StockAnalizer :
             
         self.rolling_trend[stk_id] = rolling_trend
     
-    def plot_trend_line(self, stk_id, data, features):
+    def gen_trend(self, data:pd.DataFrame, features:list[str]) -> dict[str, float]:
         
         result = {}
         for feature in features :
-            self.plot_line(data=data, x_label="years", y_label=feature)
             coef, mae = self.predict(data=data, x_label="years", y_label=feature)
-            self.save_fig(indicator_name=feature, stk_id=stk_id)
-            plt.close('all')
             result.update({f'{feature}_coef' : coef, f'{feature}_mae' : mae})
         
         return result
+
+    def plot_trend_line(self, stk_id:str, data:pd.DataFrame, features:list[str]):
+        for feature in features :
+            self.plot_line(data=data, x_label="years", y_label=feature)
+            _ , __ = self.predict(data=data, x_label="years", y_label=feature)
+            self.save_fig(indicator_name=feature, stk_id=stk_id)
+            plt.close('all')
+
     
-    def load_data(self, stk_id):
+    def load_data(self, stk_id:str) -> LoadedData:
         
-        result = []
+        result = {}
         
-        for data_type in ['dividend_history', 'profit_indicator', 'balance_sheet', 'daily_close']:
-            
+        for data_type in DATA_LIST:
             data_path = os.path.join(self.data_folder, data_type, f'{stk_id}.csv')
             data_exist = os.path.exists(data_path)
             
             if data_exist :
                 data = pd.read_csv(data_path)
             
-            result.append(data)
-        
-        return result
+            result[data_type] = data     
+        return LoadedData(**result)
     
     def get_performance(self):
         return self.performance
