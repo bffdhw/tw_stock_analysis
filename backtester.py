ae53244b6f2af38db66ffd8fb655286491bbf41e
Longer <daniel199766@gmail.com>
2023-12-30 23:02:14 +0800

    refactor : revise code

--- backtester.py
+++ backtester.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import json
 import copy
-from common import BACKTEST_START_DATE, STOP_LOSS_PCT, BACKTEST_END_DATE
+from common import BACKTEST_START_DATE, STOP_LOSS_PCT, BACKTEST_END_DATE, ADJUST_PORTFOLIO_YEAR, BUSINESS_CYCLE
 
 class Backtester:
     def __init__(self):
@@ -30,7 +30,7 @@ class Backtester:
         data = self.load_data(stk_id=stk_id)
         data = data[(backtest_start <= data['Date']) & (data['Date'] <= backtest_end)].reset_index(drop=True)
         data['Date'] = pd.to_datetime(data['Date']).dt.date
-        first_day_price = data['Close'].head(1).values[0]
+        first_day_price = data['Close'].iloc[0]
         data['updn(%)'] = self.calculate_updn_pct(target_value=data['Close'], baseline_value=first_day_price)
         return data
     
@@ -38,7 +38,7 @@ class Backtester:
     def advanced_buy_and_hold(self, target_data) -> pd.DataFrame:
         
         # strategy
-        first_day_price = target_data['Close'].head(1).values[0]
+        first_day_price = target_data['Close'].iloc[0]
         last_cost = 0.0
         stop_loss_price = 0
         position = False
@@ -50,21 +50,17 @@ class Backtester:
             # decide to buy, hold, or do nothing
             if not position and (close_price > stop_loss_price):
                 position = True
-            else :
-                pass
 
             # calculate profit
             if position:
                 profit = close_price - last_cost
-                drawdown = self.calculate_drawdown(pnl=pnl, first_day_price=first_day_price, close_price=close_price, last_cost=last_cost)
+                drawdown = self.calculate_drawdown(pnl=pnl, first_day_price=first_day_price, profit=profit)
                 # stop loss
-                if (drawdown) < STOP_LOSS_PCT:
+                if drawdown < STOP_LOSS_PCT:
                     position = False
-                    stop_loss_price = close_price
-                    last_cost = stop_loss_price         
-                # hold
-                else :
-                    last_cost = close_price
+                    stop_loss_price = close_price  
+                last_cost = close_price
+            
             else :
                 profit = 0
             
@@ -74,12 +70,12 @@ class Backtester:
         target_data['advanced_updn(%)'] = self.calculate_updn_pct(target_value=target_data['cumsum'], baseline_value=first_day_price)
         return target_data[['Date', 'advanced_updn(%)']]
     
-    def calculate_drawdown(self, pnl:list[float], first_day_price:float, close_price:float, last_cost:float)-> float:
+    def calculate_drawdown(self, pnl:list[float], first_day_price:float, profit:float)-> float:
         peaks = pd.Series(np.cumsum(pnl)).expanding().max()
         if not peaks.empty:
-            last_peak = peaks.tail(1).values[0]
+            last_peak = peaks.iloc[-1]
             last_peak_updn_pct = self.calculate_updn_pct(target_value=last_peak, baseline_value=first_day_price)
-            cumsum = np.cumsum(pnl)[-1] + (close_price - last_cost)
+            cumsum = np.cumsum(pnl)[-1] + profit
             cumsum_pct = self.calculate_updn_pct(target_value=cumsum, baseline_value=first_day_price)
             return cumsum_pct - last_peak_updn_pct
         else: 
@@ -93,22 +89,31 @@ class Backtester:
         performance = performance[portfolio_filter]   
         return list(performance['stk_id'])
     
-    def generate_portfolio(self) -> list[str]:
+    def generate_portfolio(self) -> dict[list[str]]:
         result = {}
         for backtest_year, data in self.performance.items():
             portfolio_filter = (data['net_income(%)_slope'] > 0) & (data['gross_profit(%)_slope'] > 0) & (data['revenue(%)_slope'] > 0)
-            data = data[portfolio_filter]
-            result[backtest_year] = self.select_top_n(data=copy.deepcopy(data))
+            portfolio = self.select_top_n(data=copy.deepcopy(data[portfolio_filter]))
+            result[backtest_year] = portfolio
+        
+        self.gen_portfolio_json(data=result)    
         return result
     
-    def select_top_n(self, data):
+    def gen_portfolio_json(self, data:dict[list[str]]):
+        result = {key: data[key] for key in ADJUST_PORTFOLIO_YEAR if key in data}
+        portfolio_json_path = os.path.join(self.dynamic_portfolio_path, 'portfolios.json')
+        with open(portfolio_json_path, 'w') as json_file:
+            json.dump(result, json_file)
+    
+    def select_top_n(self, data:pd.DataFrame) -> list[str]:
         data['sum_slope'] = data['net_income(%)_slope'] + data['gross_profit(%)_slope'] + data['revenue(%)_slope']
         data['sum_mae'] = data['net_income(%)_mae'] + data['gross_profit(%)_mae'] + data['revenue(%)_mae']
         data = data[['stk_id', 'sum_slope', 'sum_mae']]
+        data['stk_id'] = data['stk_id'].astype(str)
         fonts = self.non_dominated_sort(df=data)
         return fonts[0]
     
-    def non_dominated_sort(self, df):
+    def non_dominated_sort(self, df: pd.DataFrame) -> list[list[str]]:
         dominating_count = {stk_id: 0 for stk_id in df['stk_id']}
         dominated_solutions = {stk_id: [] for stk_id in df['stk_id']}
 
@@ -142,70 +147,56 @@ class Backtester:
         return fronts
     
     
-    def dynamic_portfolio_buy_and_hold(self, dynamic_portfolio) -> pd.DataFrame:
+    def dynamic_portfolio_buy_and_hold(self, dynamic_portfolio:dict[list[str]]) -> pd.DataFrame:
         
-        adjust_portfolio_year = [2012, 2017, 2022]
-        portfolios = {}
-        cumsum_profit = pd.DataFrame()
-        advanced_cumsum_profit = pd.DataFrame()
+        result = pd.DataFrame()
         last_period_cumsum = 0
         last_advanced_period_cumsum = 0
         
-        for backtest_start_year in adjust_portfolio_year :
+        for backtest_start_year in ADJUST_PORTFOLIO_YEAR :
+            period_performance_path = os.path.join(self.dynamic_portfolio_path, f'{backtest_start_year}')
+            os.makedirs(period_performance_path, exist_ok=True)
+            
             period_cumsum_profit = pd.DataFrame()
             period_advanced_cumsum_profit = pd.DataFrame()
             portfolio = dynamic_portfolio[backtest_start_year]
-            portfolios[backtest_start_year] = portfolio
-            
-            period_performance_path = os.path.join(self.dynamic_portfolio_path, f'{backtest_start_year}')
-            os.makedirs(period_performance_path, exist_ok=True)
             print(portfolio)
             
-            benchmark_data = self.process_data(stk_id='0050', backtest_start=f'{backtest_start_year}-04-01', backtest_end=f'{backtest_start_year+5}-03-31')
+            start_date = f'{backtest_start_year}-04-01'
+            end_date   = f'{backtest_start_year + BUSINESS_CYCLE}-03-31'
+            
+            benchmark_data = self.process_data(stk_id='0050', backtest_start=start_date, backtest_end=end_date)
             benchmark_data = benchmark_data[['Date', 'updn(%)']]
             benchmark_data.columns = ['Date','benchmark_updn(%)']
             
             for stk_id in portfolio:
-                target_data = self.process_data(stk_id=stk_id, backtest_start=f'{backtest_start_year}-04-01', backtest_end=f'{backtest_start_year+5}-03-31')
-                
+                target_data = self.process_data(stk_id=stk_id, backtest_start=start_date, backtest_end=end_date)
                 updn = target_data['updn(%)']
                 advanced_updn = self.advanced_buy_and_hold(target_data=target_data)['advanced_updn(%)']
                 period_cumsum_profit[stk_id] = updn
                 period_advanced_cumsum_profit[stk_id] = advanced_updn
                 
                 # plot stock performance
-                period_stock_performance = pd.DataFrame()
-                period_stock_performance['updn(%)'] = updn
-                period_stock_performance['advanced_updn(%)'] = advanced_updn
-                period_stock_performance['Date'] = target_data['Date']
+                period_stock_performance = pd.DataFrame({'updn(%)': updn, 'advanced_updn(%)': advanced_updn, 'Date': target_data['Date']})
                 period_stock_performance = pd.merge(period_stock_performance, benchmark_data, how='left', on=['Date']).dropna()
                 self.plot_performance(data=period_stock_performance, columns=['updn(%)' , 'advanced_updn(%)', 'benchmark_updn(%)'], title=f'{stk_id}', path=period_performance_path)
             
-            period_cumsum_profit['portfolio'] = period_cumsum_profit.sum(axis=1) / len(portfolio) + last_period_cumsum
-            period_cumsum_profit['advanced_portfolio'] = period_advanced_cumsum_profit.sum(axis=1) / len(portfolio) + last_advanced_period_cumsum
-            period_cumsum_profit["Date"] = target_data['Date']
-            period_cumsum_profit = pd.merge(period_cumsum_profit, benchmark_data, how='left', on=['Date']).dropna()
+            portfolio_updn = period_cumsum_profit.sum(axis=1) / len(portfolio) + last_period_cumsum
+            advanced_portfolio_updn = period_advanced_cumsum_profit.sum(axis=1) / len(portfolio) + last_advanced_period_cumsum
+            period_result = pd.DataFrame({'portfolio': portfolio_updn, 'advanced_portfolio': advanced_portfolio_updn, 'Date': target_data['Date']})
             
             # plot period performance
-            plot_data = copy.deepcopy(period_cumsum_profit)
+            plot_data = copy.deepcopy(period_result)
+            plot_data = pd.merge(plot_data, benchmark_data, how='left', on=['Date']).dropna()
             plot_data['portfolio'] = plot_data['portfolio'] - last_period_cumsum
             plot_data['advanced_portfolio'] = plot_data['advanced_portfolio'] - last_advanced_period_cumsum
             self.plot_performance(data=plot_data, columns=['portfolio' , 'advanced_portfolio', 'benchmark_updn(%)'], title='period_portfolio', path=period_performance_path)
             
-            cumsum_profit = pd.concat([cumsum_profit, period_cumsum_profit[['Date', 'portfolio']]])
-            last_period_cumsum = cumsum_profit['portfolio'].tail(1).values[0]
-            advanced_cumsum_profit = pd.concat([advanced_cumsum_profit, period_cumsum_profit[['Date', 'advanced_portfolio']]])
-            last_advanced_period_cumsum = advanced_cumsum_profit['advanced_portfolio'].tail(1).values[0]
-        cumsum_profit = cumsum_profit.reset_index(drop=True)
-        advanced_cumsum_profit = advanced_cumsum_profit.reset_index(drop=True)
-        
-        portfolio_json_path = os.path.join(self.dynamic_portfolio_path, 'portfolios.json')
-        with open(portfolio_json_path, 'w') as json_file:
-            json.dump(portfolios, json_file)
-        
-        result = pd.merge(cumsum_profit, advanced_cumsum_profit, how='left', on=['Date']).dropna()
-        result = pd.merge(result, self.benchmark_data, how='left', on=['Date']).dropna()
-        return result
+            last_period_cumsum = period_result['portfolio'].iloc[-1]
+            last_advanced_period_cumsum = period_result['advanced_portfolio'].iloc[-1]
+            
+            result = pd.concat([result, period_result])
+        return pd.merge(result, self.benchmark_data, how='left', on=['Date']).dropna()
     
     
     def load_performance(self) -> dict[int, pd.DataFrame]:
