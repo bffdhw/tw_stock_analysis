80aa5ffa35bb78a962fe0afcbd791ca062f8193b
Longer <daniel199766@gmail.com>
2023-12-31 17:24:06 +0800

    feat : check if the file is already exist

--- yahoo_finance_client.py
+++ yahoo_finance_client.py
@@ -1,33 +1,35 @@
 import os
 import time
 import yfinance as yf
-import tw_stock_id
+from common import STK_LIST
 
 
 class YahooFinanceClient:
     
-    def __init__(self):
+    def __init__(self, industry:str):
         self.data_folder = os.path.abspath("./data")
         os.makedirs(self.data_folder, exist_ok=True)
+        self.stk_list = STK_LIST[industry]
     
-    def get_raw_daily_close(self, stk_id):
+    def get_raw_daily_close(self, stk_id, daily_close_folder):
         df = yf.download(f'{stk_id}.TW')
         df = df['Close']
         df = df.reset_index()
+        df.to_csv(os.path.join(daily_close_folder, f'{stk_id}.csv'), index=False)
+        
+    def get_raw_data(self):
         
         daily_close_folder = os.path.join(self.data_folder, 'daily_close')
         if not os.path.exists(daily_close_folder):
             os.makedirs(daily_close_folder)
         
-        df.to_csv(os.path.join(daily_close_folder, f'{stk_id}.csv'), index=False)
-        
-    def get_raw_data(self, stk_list):
-        for stk_id in stk_list:
-            self.get_raw_daily_close(stk_id=stk_id)
-            time.sleep(1)
-
+        for stk_id in self.stk_list:
+            file_exists = os.path.exists(os.path.join(daily_close_folder, f'{stk_id}.csv'))
+            if not file_exists:
+                self.get_raw_daily_close(stk_id=stk_id, daily_close_folder=daily_close_folder)
+                time.sleep(1)
 
 if __name__ == '__main__':
-    stk_list = tw_stock_id.SEMICONDUCTOR
+    industry = 'electronic_components'
     client = YahooFinanceClient()
-    client.get_raw_data(stk_list=stk_list)
+    client.get_raw_data(industry=industry)
